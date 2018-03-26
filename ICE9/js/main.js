880c879aac26b67827c3c3ed4dd9abe5d1474d06
Markinsane <markinsane@github.com>
2018-03-26 11:17:13 -0400

    commit

--- ICE9/js/main.js
+++ ICE9/js/main.js
@@ -1,8 +1,13 @@
 var card=$(".card")
 var carousel=$(".container-fluid")
 
+
 TweenMax.staggerFrom(card, 1, {y:100, opacity:0, delay:0.5}, 0.2);
 TweenMax.from(carousel, 1, {y:100, opacity:0});
+
+
+
+
 
 
 
